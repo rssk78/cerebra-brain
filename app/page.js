566f5187850e63016c662cbce5d4c64383abc25e
rssk78 <68418535+rssk78@users.noreply.github.com>
2026-04-13 22:19:31 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@
     <title>Cerebra Brain - Landing Page</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="global.css">
 </head>
 <body>
     <header>
